6bab3654da3d5f55e3c9bfd4df4e103c235802a0
SaraMColom <scolom@umich.edu>
2019-10-30 10:34:30 -0400

    Adding figures and code update 103019

--- Code/DataExploration.py
+++ Code/DataExploration.py
@@ -97,7 +97,7 @@ EdDesertCBSA_2=EdDesertCBSA_2.merge(GDP, on='CBSA_Title')
 EdDesertCBSA_2['EducationDesert']=EdDesertCBSA_2.EducationD.apply(lambda set_: 'Desert' if set_==0 else "NotDesert")
 
 # pivot and plot
-EdDesertCBSA_2.pivot(columns="EducationDesert", values="Percent change in real GDP by metropolitan area").plot.hist(bins=100, alpha = 0.5)
+EdDesertCBSA_2.pivot(columns="EducationDesert", values="PerChangeGDP").plot.hist(bins=100, alpha = 0.5)
 plt.xlabel("GDP change %", labelpad=14)
 plt.title("Percent change in real GDP by metropolitan area",y=1.02)
 plt.show()
@@ -109,8 +109,85 @@ fig, ax = plt.subplots(figsize=(14, 6))
 fig=EdDesertCBSA_2[EdDesertCBSA_2.EducationD==0].plot(color="yellow",ax=ax) # Plot deserts yellow
 EdDesertCBSA_2[EdDesertCBSA_2.EducationD==1].plot(color="lightblue",ax=ax) # Plot non deserts blue
 ax.set_title('Education Deserts in the US Based on CBSA',fontdict={'fontsize': '25','fontweight': '3'}) # add title
-ax.annotate('Source: TIGER.gov',xy=(0.1, .08),  xycoords='figure fraction', horizontalalignment='left', verticalalignment='top', fontsize=12, color='black') # add data source
+ax.annotate('Data source: shape.file from TIGER.gov',xy=(0.1, .08),  xycoords='figure fraction', horizontalalignment='left', verticalalignment='top', fontsize=12, color='black') # add data source
 
 plt.show()
 
+# Pairwise correlation between variables
+# Seaborn visualization library
+import seaborn as sns# Create the default pairplot
+
+Subset=pd.DataFrame(EdDesertCBSA_2[['PerChangeGDP', 'NatResMining', 'Constr', 'DurGoManu','NonGoodManu', 'Trade', 'TransUtil','EducationDesert']])
+       
+sns.pairplot(Subset,hue='EducationDesert')
+
+plt.show()
+
+
+
 # Plot map chloropleth by GDP and then school points on top.
+StatesShape = gpd.read_file("/Users/sara/Desktop/MDST/education-deserts/CleanData/states_21basic/states.shp")
+StatesShape.head()
+
+fig, ax = plt.subplots(figsize=(14, 6))
+## first layer, all geometries included 
+fig=StatesShape.plot(color="lightgrey",ax=ax) # Plot the US as grey
+EdDesertCBSA_2.plot(ax=ax,column='PerChangeGDP',scheme="quantiles",legend=True,legend_kwds={'loc': 'lower left'}) # Fill in states with information by amount of pesticide applied, NOTE LEGEND LOCATION SPECIFIED HERE
+SchNum.plot(ax=ax,color='red',markersize=0.1)
+
+plt.show()
+
+# PCA 
+
+EdDesertFull=EdDesertCBSA_2.dropna()
+
+from sklearn.preprocessing import StandardScaler
+features = ['PerChangeGDP', 'NatResMining', 'Constr', 'DurGoManu',
+       'NonGoodManu', 'Trade', 'TransUtil', 'Info',
+       'FinInsRealEstRentLea', 'ProBusiServi', 'EdSerHealthSociAs',
+       'ArtEntRec', 'OtherServices', 'Govern']# Separating out the features
+x = EdDesertFull.loc[:, features].values# Separating out the target
+y = EdDesertFull.loc[:,['EducationDesert']].values# Standardizing the features
+x = StandardScaler().fit_transform(x)
+
+
+from sklearn.decomposition import PCA
+pca =PCA(n_components=3)
+principalComponents = pca.fit_transform(x)
+
+principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2','principal component 3'])
+
+finalDf = pd.concat([principalDf, EdDesertFull[['EducationDesert']]], axis = 1)
+
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1) 
+
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 component PCA', fontsize = 20)
+targets = ['Desert', 'NotDesert']
+colors = ['b','orange']
+
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['EducationDesert'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c = color
+               , s = 50)
+ax.legend(targets)
+ax.grid('off')
+
+plt.show()
+
+
+var= pca.explained_variance_ratio_
+
+
+#Explained variance
+pca = PCA().fit(x)
+plt.plot(np.cumsum(pca.explained_variance_ratio_))
+plt.xlabel('number of components')
+plt.ylabel('cumulative explained variance')
+plt.show()
+
+### Linear regression modeling
